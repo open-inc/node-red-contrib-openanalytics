25e0b5ba13ee19611af81f8878be05fd03749d38
Martin <stein@openinc.de>
2024-10-17 09:43:47 +0200

    Version 1.06
    - Added new Nodes
    - Fixed TS import bug

--- src/nodes/openware-items/openware-items.ts
+++ src/nodes/openware-items/openware-items.ts
@@ -2,7 +2,7 @@ import { NodeInitializer } from "node-red";
 import { OpenwareItemsNode, OpenwareItemsNodeDef } from "./modules/types";
 import { ConfigNode, ItemMessage, OWItemType } from "../shared/types";
 import { ItemsMsgType } from "./shared/types";
-import { isError } from "../shared/helper";
+import { isError } from "../shared/Helper";
 
 const nodeInit: NodeInitializer = (RED): void => {
   function OpenwareItemsNodeConstructor(
